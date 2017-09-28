1f08c8da63b81953548615afe112e6f46d562101
Ted Wu <ted.wu@chartboost.com>
2017-09-27 17:02:52 -0700

    adding a new form

--- server.py
+++ server.py
@@ -16,6 +16,21 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
+            if self.path.endswith('/restaurants/new'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
@@ -58,9 +73,13 @@ class webServerHandler(BaseHTTPRequestHandler):
         try:
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_headers(self.headers.getheader('content-type'))
-                if ctype === 'multipart/form-data':
+                print pdict
+                print self.headers.getheader('content-type')
+                if ctype == 'multipart/form-data':
                     fields = cgi.parse_multipart(self.rfile, pdict)
                     #- rfile is a file object open for reading positioned at the start of the optional input data part;
+        except:
+            pass
 
 def main():
     try:
